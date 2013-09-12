88772c7395f16a6e01445ec85a351ea9f756330c
Alexandre Gama <alexandre.gama.lima@gmail.com>
2013-09-12 16:26:06 -0300

    Update IndexController.java

--- src/main/br/com/caelum/leilao/controller/IndexController.java
+++ src/main/br/com/caelum/leilao/controller/IndexController.java
@@ -4,7 +4,7 @@ import br.com.caelum.vraptor.Path;
 import br.com.caelum.vraptor.Resource;
 
 @Resource
-public class IndexController a {
+public class IndexController {
 
 	@Path("/")
 	public void index() {
